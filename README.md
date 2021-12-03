970309e95d61644119f886ad662144651e3e017b
Andrew Lee <tinpo@dhcp-10-250-67-171.harvard.edu>
2021-12-03 12:42:26 -0500

    updates

--- README.md
+++ README.md
@@ -4,12 +4,28 @@ CS50 Final Project - Kaitlin Lampson, Ella Lee, Priyanka Kaul
 Pages:
 1. index (HOME -- about)
 2. register
+    - from finance 
+    - no username (just email and password)
+        - require email to have @college.harvard.edu or @fas.harvard.edu
+        - require password to have a certain amount of characters/special characters ... something
 3. log in
+    - from finance
 4. search textbooks
+    - search box
     - potential API (google books)
+    - be able to favorite certain textbooks
 5. add textbooks
-6. history
-7. "shopping cart" or "favorites" or "library"
+    - should be similar to register page I think
+        - connect title to Google Books API description
+        - add condition of book
+        - an option for a comment
+6. My textbooks
+    - displays posted textbooks
+    - allows you to take down post
+7. "Favorites"
+    - favorited textbooks(as well as their information) pop up here
+    - email pops up once book is favorited
+    - if book is unfavorited, email goes away
 
 Ella: logo/name
 
--- app.py
+++ app.py
@@ -24,10 +24,37 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-# db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///bookswap.db")
+
+# Create new table, and index
+db.execute("CREATE TABLE IF NOT EXISTS orders (id INTEGER, user_id NUMERIC NOT NULL, title TEXT NOT NULL, \
+            timestamp TEXT, PRIMARY KEY(id), \
+            FOREIGN KEY(user_id) REFERENCES users(id))")
+db.execute("CREATE INDEX IF NOT EXISTS orders_by_user_id_index ON orders (user_id)")
+
 
 @app.route("/")
 # @login_required
 def index():
 
     return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register User"""
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    # Check username and password
+    email = request.form.get("email")
+    password = request.form.get("password")
+    confirmation = request.form.get("confirmation")
+
+    # Add users to bookswap.db
+    db.execute('INSERT INTO users (email, hash) \
+        VALUES(?,?)', email, generate_password_hash(password))
+    
+    rows = db.execute("SELECT * FROM users WHERE email = ?", email)
+    session["user_id"] = rows[0]["id"]
+    return redirect("/")
+
